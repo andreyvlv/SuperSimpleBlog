44e377ace06bc4a1eea199f257ebd548d1af029a
Андрей Власов <andrey11vlasov@yandex.ru>
2024-01-10 15:53:24 +0300

    Update to NET 8

--- SuperSimpleBlog/Helpers/RoleInitializer.cs
+++ SuperSimpleBlog/Helpers/RoleInitializer.cs
@@ -11,8 +11,9 @@ namespace SuperSimpleBlog.Helpers
         public static async Task InitializeAsync(UserManager<IdentityUser> userManager, RoleManager<IdentityRole> roleManager)
         {
             // Initialization admin role and user if that is not iniatialized
-            string adminEmail = "admin@admin.com";
-            string password = "123Qwe!!";
+            string adminEmail = "admin@admin.com"; // standart login for initialization
+            string password = "123Qwe!!"; // standart password for initialization
+
             if (await roleManager.FindByNameAsync("admin") == null)
             {
                 await roleManager.CreateAsync(new IdentityRole("admin"));
